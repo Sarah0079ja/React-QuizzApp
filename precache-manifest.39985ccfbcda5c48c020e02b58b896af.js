110fa89226cb505d9bb091b3677a1f357cc6478e
Sarah0079ja <sodiavbara@gmail.com>
2019-11-13 05:00:15 +0000

    Updates

---
+++ precache-manifest.39985ccfbcda5c48c020e02b58b896af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1ae49ae8f6e667c5d38c582eb318d3",
+    "url": "/React-quizzapp/index.html"
+  },
+  {
+    "revision": "12da26f58074c076f398",
+    "url": "/React-quizzapp/static/css/main.b64bfe59.chunk.css"
+  },
+  {
+    "revision": "5dc7a68e07c9de5334eb",
+    "url": "/React-quizzapp/static/js/2.2bed640f.chunk.js"
+  },
+  {
+    "revision": "12da26f58074c076f398",
+    "url": "/React-quizzapp/static/js/main.8015e830.chunk.js"
+  },
+  {
+    "revision": "3a2ba55b70295457e29a",
+    "url": "/React-quizzapp/static/js/runtime-main.3d6cc79e.js"
+  }
+]);
